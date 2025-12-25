ccfe42775c175750654336604a4d98d92c84b7e4
Ronald Huereca <ronald@mediaron.com>
2025-12-25 01:31:31 -0600

    Doing build.

--- build/gb-extras-block-labels.asset.php
+++ build/gb-extras-block-labels.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-hooks'), 'version' => 'f3dd3c4192bc00a0aa2d');
+<?php return array('dependencies' => array('wp-blocks', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-hooks'), 'version' => '7322f500ddfbf495615f');
