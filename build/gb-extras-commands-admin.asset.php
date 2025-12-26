8a5bf0c082737ffd8b7e71d56b1ae290edc11165
Ronald Huereca <ronald@mediaron.com>
2025-12-25 18:21:25 -0600

    Doing GB Extras prod build.

--- build/gb-extras-commands-admin.asset.php
+++ build/gb-extras-commands-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-commands', 'wp-components', 'wp-i18n', 'wp-plugins'), 'version' => '0fa0b34677f22f65a3f4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-commands', 'wp-components', 'wp-i18n', 'wp-plugins'), 'version' => '640097273af9ed738598');
--- build/gb-extras-commands-frontend.asset.php
+++ build/gb-extras-commands-frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-commands', 'wp-components', 'wp-i18n', 'wp-plugins'), 'version' => '1edaedbd58174b08d36f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-commands', 'wp-components', 'wp-i18n', 'wp-plugins'), 'version' => 'fa0bfb798312078e3b3d');
